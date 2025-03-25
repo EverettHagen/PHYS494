399623a9bb2ea18042b4d84ac572cef6ba5c6657
EverettHagen <ehagen2@vols.utk.edu>
2025-03-25 11:58:04 -0400

    Update Flicker_LED.ino
    Added Comments

--- Flicker_LED.ino
+++ Flicker_LED.ino
@@ -1,27 +1,27 @@
 #include <FastLED.h>
 
-#define LED_PIN 8
-#define NUM_LEDS 48  // Change this based on the number of LEDs you have
+#define LED_PIN 8    // The signal output pin
+#define NUM_LEDS 48  // The number of LEDs you have
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  randomSeed(analogRead(0)); // Initialize random seed
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  int numToTrigger = random(1, NUM_LEDS + 1);  // Random number of LEDs to trigger sequentially
+  int numToTrigger = random(1, NUM_LEDS + 1); 
 
   for (int i = 0; i < numToTrigger; i++) {  
-    leds[i] = CHSV(219, 58, 93);  // Turn on LED
+    leds[i] = CHSV(219, 58, 93);  //Inside CHSV() can change the colors according to HSV Color model
     FastLED.show();
-    delay(100);  // Delay between each LED lighting up
+    delay(100);  // How long till next LED turns on
 
-    leds[i] = CHSV(0, 0, 0);  // Turn off LED after delay
+    leds[i] = CHSV(0, 0, 0); 
     FastLED.show();
-    delay(200);
+    delay(200);  // How long LED stays on
   }
 
-  delay(random(0, 2000));  // Random pause before next cycle
+  delay(random(0, 2000));  // Pause before next cycle
 }
